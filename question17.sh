e537bb76e18350b3198d2bbb389fe7e0ce23904a
Zilin Song <121729106+codingzilin@users.noreply.github.com>
2023-08-05 21:12:39 +0930

    Update question17.sh

--- question17.sh
+++ question17.sh
@@ -1,15 +1,20 @@
-# Clone the repository.
-#git clone https://github.com/ian-knight-uofa/git-practice-03.git
+# Clone the repository
+git clone https://github.com/ian-knight-uofa/git-practice-03.git
 
-# Create the branches branch1 and branch2.
+# Create the branches branch1 and branch2
 git checkout -b branch1
 git checkout -b branch2
 
-# Add, move, remove, and modify files to match the configurations shown above.
-echo "This is a new file" > newfile1
+# In the main branch, add the file newfile1
 git add newfile1
 git commit -m "Added newfile1"
-echo "This is a modified foo file" > dir1/dir2/foo
-echo "This is a new file" > dir3/newfile2
+
+# In the branch1 branch, remove the file dir2/foo and add the file newfile1
+git rm dir2/foo
+git add newfile1
+git commit -m "Removed dir2/foo and added newfile1"
+
+# In the branch2 branch, move the file dir2/foo to dir1/foo_modified and add the file newfile2
+git mv dir2/foo dir1/foo_modified
 git add newfile2
-git commit -m "Modified foo file and added newfile2"
+git commit -m "Moved dir2/foo to dir1/foo_modified and added newfile2"
