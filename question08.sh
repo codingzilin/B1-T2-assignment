583cf8181689e95f1ac1f14eb6c60c3ab121b887
Zilin Song <121729106+codingzilin@users.noreply.github.com>
2023-08-15 20:52:09 +0930

    Update question08.sh

--- question08.sh
+++ question08.sh
@@ -3,13 +3,11 @@ mkdir dir2
 
 # Move all text files to dir2
 for file in *.txt; do
-  if [ -f "$file" ]; then
     mv "$file" dir2
-  fi
 done
 
 # Stage the changes
-git add dir2
+git add .
 
 # Commit the changes
 git commit -m "Moved all text files to dir2"
